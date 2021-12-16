63fe184ba9d6dbd10f42c77c18c5ee6d81597ec0
Tim Yalovoy <tim_yalovoy@mail.ru>
2021-12-16 21:29:44 +0400

    Add Board.cs script

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private float _height;
+    private float _width;
+
+    public float Height {
+        get => _height;
+        set => _height = value;
+    }
+
+    public float Width
+    {
+        get => _width;
+        set => _width = value;
+    }
+
+    public PoolObject poolObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // cache of data
+        poolObject = FindObjectOfType<PoolObject>();
+
+        this.Height = 7;
+        this.Width = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
