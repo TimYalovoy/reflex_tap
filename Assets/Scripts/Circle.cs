65416d57b3f47cb39bb503c71c31c0e4ab76b112
Tim Yalovoy <tim_yalovoy@mail.ru>
2021-12-18 17:52:43 +0400

    Update Circle.cs

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -1,78 +1,104 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Circle : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject go;
-    [SerializeField]
-    private Transform tf;
+    public GameObject go;
+    public Transform trfm;
 
-    public bool delayTimerIsDone = false;
-    public float secondsDelay = 3f;
+    private bool delayTimerIsDone = false;
+    private bool reflexTimerIsDone = false;
 
-    public bool reflexTimerIsDone = false;
-    public float secondsReflex = 3f;
-
-    private float coef;
-    void Start()
+    private float _secondsDelay;
+    public float SecondsDelay
     {
-        // кэширование данных
-        go = this.gameObject;
-        tf.localScale = go.GetComponent<Transform>().localScale;
-
-        try {
-            coef = 1f / secondsReflex;
-        } catch (Exception e)
-        {
-            Debug.Log($"Error:{e}");
-            coef = 1f;
-        }
+        get => _secondsDelay;
+        set => _secondsDelay = value;
+    }
+    private float _secondsReflex;
+    public float SecondsReflex
+    {
+        get => _secondsReflex;
+        set => _secondsReflex = value;
     }
 
-    void Update()
+    private float coef;
+
+    private void Awake()
     {
-        
+        go = this.gameObject;
+        trfm.localScale = go.GetComponent<Transform>().localScale;
+    }
+
+    void Start()
+    {
+        ResetState();
+        if (SecondsReflex == 0)
+        {
+            coef = 1f;
+        }else{
+            coef = 1f / SecondsReflex;    
+        }
     }
 
     private void FixedUpdate()
     {
         if (!delayTimerIsDone)
         {
-            if (secondsDelay > 0)
+            if (SecondsDelay > 0)
             {
-                secondsDelay -= Time.deltaTime;
+                SecondsDelay -= Time.deltaTime;
             }
             else
             {
-                secondsDelay = 0;
+                SecondsDelay = 0;
                 delayTimerIsDone = true;
             }
         }
         else if (!reflexTimerIsDone)
         {
-            if (secondsReflex > 0)
+            if (SecondsReflex > 0)
             {
-                secondsReflex -= Time.deltaTime;
-                tf.localScale = new Vector3(tf.localScale.x - (Time.deltaTime * coef), tf.localScale.y - (Time.deltaTime * coef));
+                SecondsReflex -= Time.deltaTime;
+                trfm.localScale = new Vector3(
+                    trfm.localScale.x - (Time.deltaTime * coef),
+                    trfm.localScale.y - (Time.deltaTime * coef)
+                    );
             }
             else
             {
-                secondsReflex = 0;
-                tf.localScale = new Vector3(0.00f, 0.00f);
                 go.SetActive(false);
+                trfm.localScale = new Vector3(0.00f, 0.00f);
+                SecondsReflex = 0;
                 reflexTimerIsDone = true;
+                ResetState();
             }
         }
     }
 
     public void ResetState()
     {
+        trfm.localScale = new Vector3(1f, 1f);
+        trfm.position = new Vector3(
+            Random.Range(-2f, 2f), // x
+            Random.Range(-4f, 4f)  // y
+            );
+
         delayTimerIsDone = false;
         reflexTimerIsDone = false;
 
+        this.SecondsDelay = 1f;
+        this.SecondsReflex = 1.25f;
+
         go.SetActive(true);
+    }
+
+    public void OnMouseDown()
+    {
+        delayTimerIsDone = true;
+        reflexTimerIsDone = true;
+        go.SetActive(false);
+        ResetState();
     }
 }
