cf29db084c3f629e788ef5b4e0d41380e6456018
Tim Yalovoy <tim_yalovoy@mail.ru>
2021-12-20 22:58:58 +0400

    Update Data.cs script

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -4,41 +4,6 @@ using UnityEngine;
 
 public class Data : MonoBehaviour
 {
-    // Board data
-    #region Height
-    [SerializeField]
-    private float _maxHeight = 3f;
-    public float MaxHeight
-    {
-        get => _maxHeight;
-        set => _maxHeight = value;
-    }
-    [SerializeField]
-    private float _minHeight = -4f;
-    public float MinHeight
-    {
-        get => _minHeight;
-        set => _minHeight = value;
-    }
-    #endregion
-
-    #region Width
-    [SerializeField]
-    private float _maxWidth = 2f;
-    public float MaxWidth
-    {
-        get => _maxWidth;
-        set => _maxWidth = value;
-    }
-    [SerializeField]
-    private float _minWidth = -2f;
-    public float MinWidth
-    {
-        get => _minWidth;
-        set => _minWidth = value;
-    }
-    #endregion
-
     // Circle data
     #region Time (seconds)
     [SerializeField]
@@ -54,6 +19,4 @@ public class Data : MonoBehaviour
         get => _timeToCollapse;
     }
     #endregion
-
-
 }
