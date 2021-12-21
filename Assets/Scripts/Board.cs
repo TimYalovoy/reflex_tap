0d923966c10b06dd3b8a751913029c5c1be52dc0
Tim Yalovoy <tim_yalovoy@mail.ru>
2021-12-21 22:55:10 +0400

    Update Board.cs script

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -11,6 +11,7 @@ public class Board : MonoBehaviour
     #region Score
     private int circleCost = 1;
     private int score = 0;
+    [SerializeField]
     private int clicks = 0;
     public TextMesh scoreLabel;
     #endregion
@@ -61,13 +62,15 @@ public class Board : MonoBehaviour
     public void ScoreIncrease()
     {
         clicks++;
-        _ = clicks > 10 ? score += circleCost*clicks : score += circleCost;
+        _ = clicks > 10 ? score += circleCost * 2 : score += circleCost;
         scoreLabel.text = score.ToString();
+        time2play += 1.05f;
     }
     public void ScoreDecrease()
     {
         _ = clicks > 10 ? clicks = 10 : clicks = 0;
-        score -= circleCost*20;
+        score -= circleCost*3;
         scoreLabel.text = score.ToString();
+        time2play -= 1f;
     }
 }
